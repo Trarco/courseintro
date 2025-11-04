a073ea717159658ff916765cba631d2391d40263
Trarco <trarco22@outlook.it>
2025-11-04 19:03:29 +0100

    Upgrade Version

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_courseintro';         // Full name of the plugin.
-$plugin->version   = 2025102300;                  // Data versioning: YYYYMMDDXX.
+$plugin->version   = 2025110400;                  // Data versioning: YYYYMMDDXX.
 $plugin->requires  = 2022041900;                  // Requires this Moodle version (e.g. Moodle 4.0).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.4';
+$plugin->release   = '1.0.5';
